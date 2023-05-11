3efa0f8635500e0193687738467bf6bd3770cd6d
mohammadtaherri <taherimohammad08@gmail.com>
2023-05-11 19:30:38 +0330

    create text_wrapper project

---
+++ text_wrapper/bin/text_wrapper.dart
@@ -0,0 +1,5 @@
+import 'package:text_wrapper/text_wrapper.dart' as text_wrapper;
+
+void main(List<String> arguments) {
+  print('Hello world: ${text_wrapper.calculate()}!');
+}
---
+++ text_wrapper/test/text_wrapper_test.dart
@@ -0,0 +1,8 @@
+import 'package:text_wrapper/text_wrapper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
